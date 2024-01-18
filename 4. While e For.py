2e12cbeed0d826fd794da89c224951557fd49bde
Hosana09 <hosanapires9@gmail.com>
2024-01-17 21:11:29 -0300

    Alteração no arquivo 4. While e For

--- 4. While e For.py
+++ 4. While e For.py
@@ -4,46 +4,43 @@
 ###################################################
 
 # Inicialização das variáveis
-
-N = int(input()) #quantidade de números que terá na lista 
+N = int(input("Digite a quantidade de número que terá a lista: ")) #quantidade de números que terá na lista 
 lista = []
 
 
 # Leitura da sequência de números
-
 for i in range(0,N):
-    X = int(input()) #qual número vai ser efetuada a equação
+    X = int(input("Digite o número para ser efetuada a equação: ")) #qual número vai ser efetuada a equação
     lista.append (X)
 
-#print (lista)
+print("A sequência para realizar a Conjectura de Collatz é:", lista)
 
 for X in lista:
     listax = [X]
     pares = 0
-    impar = 0
-    Xin = X    
+    impares = 0
+    Xin = X  
+    if (X % 2 == 0):
+        pares = pares + 1 
+    else:
+        impares = impares + 1
 
     while (X != 1):
         if (X % 2 == 0):
             X = X / 2
             pares = pares + 1 
+            listax.append(int(X))
         else:
             X = (3 * X) + 1
-            impar = impar + 1
-        listax.append(int(X))
-
-    if (X % 2 == 0):
-        pares = pares + 1 
-    else:
-        impar = impar + 1
-        
-    #print(listax)
+            impares = impares + 1
+            listax.append(int(X))
+    
+#print(listax)
     MAX = max(listax)
 
 # Impressão das informações de saída
-
+    print("Para o número", Xin, "temos a sequência:", listax)  
     print("Valor inicial:", Xin)
-    print("Numeros Pares:", pares)
-    print("Numeros Impares:", impar)
-    print("Maior Numero:", MAX)
-
+    print("Quantidade de números pares:", pares)
+    print("Quantidade de números ímpares:", impares)
+    print("Maior número:", MAX)
