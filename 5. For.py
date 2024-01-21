9a6f574256db48dafcb69103ac2c838e37a734f4
Hosana09 <hosanapires9@gmail.com>
2024-01-20 21:00:12 -0300

    Alteração nos arquivos 5. For e 6. For Eleição.

--- 5. For.py
+++ 5. For.py
@@ -4,15 +4,15 @@
 ###################################################
 
 # Leitura da vida de Jinx e Ekko
-
-hp_jinx = int(input()) #pontos de vida Jinx
-hp_ekko = int(input()) #pontos de vida Ekko
+print("Bem vindo ao jogo Batalha na Ponte de Piltover!")
+hp_jinx = int(input("Digite a quantidade de pontos de vida da Jinx: ")) #pontos de vida Jinx
+hp_ekko = int(input("Digite a quantidade de pontos de vida do Ekko: ")) #pontos de vida Ekko
 
 # Leitura dos ataques da Jinx
-quantidade_ataque_jinx = int(input()) #quantidade de ataques Jinx
+quantidade_ataque_jinx = int(input("Digite a quantidade de ataques da Jinx: ")) #quantidade de ataques Jinx
 
 for n in range (0, quantidade_ataque_jinx):
-    valor_ataque_jinx = int(input())
+    valor_ataque_jinx = int(input("Qual o valor desse ataque da Jinx? "))
     if (hp_jinx < hp_ekko):
         valor_ataque_jinx = valor_ataque_jinx // 2
         hp_ekko = hp_ekko - valor_ataque_jinx
@@ -22,10 +22,10 @@ for n in range (0, quantidade_ataque_jinx):
         break
 
 # Leitura dos ataques do Ekko
-quantidade_ataque_ekko = int(input())
+quantidade_ataque_ekko = int(input("Digite a quantidade de ataques do Ekko: "))
             
 for i in range (0, quantidade_ataque_ekko):
-    valor_ataque_ekko = int(input())
+    valor_ataque_ekko = int(input("Qual o valor desse ataque do Ekko? "))
     if (hp_ekko <= 0):
         break
     if (hp_ekko < hp_jinx):
@@ -36,21 +36,21 @@ for i in range (0, quantidade_ataque_ekko):
 
 # Impressão das informações de saída
 if (hp_ekko > 0 and hp_jinx > 0):
-    print('Vida da Jinx:', hp_jinx)
-    print('Vida do Ekko:', hp_ekko)
+    print("Vida da Jinx:", hp_jinx)
+    print("Vida do Ekko:", hp_ekko)
 elif (hp_ekko == 0 and hp_jinx == 0):
-    print('Vida da Jinx:', 0)
-    print('Vida do Ekko:', 0)
+    print("Vida da Jinx:", 0)
+    print("Vida do Ekko:", 0)
 elif (hp_ekko <= 0):
-    print('Vida da Jinx:', hp_jinx)
-    print('Vida do Ekko:', 0)
+    print("Vida da Jinx:", hp_jinx)
+    print("Vida do Ekko:", 0)
 elif (hp_jinx <= 0):
-    print('Vida da Jinx:', 0)
-    print('Vida do Ekko:', hp_ekko)
+    print("Vida da Jinx:", 0)
+    print("Vida do Ekko:", hp_ekko)
 
 if (hp_jinx > hp_ekko):
-    print('Jinx foi a vencedora da batalha')
+    print("Jinx foi a vencedora da batalha!")
 elif (hp_ekko > hp_jinx):
-    print('Ekko foi o vencedor da batalha')
+    print("Ekko foi o vencedor da batalha!")
 else:
-    print('A batalha terminou empatada')
+    print("A batalha terminou empatada...")
--- 6. For Eleição.py
+++ 6. For Eleição.py
@@ -4,45 +4,53 @@
 ###################################################
 
 # Leitura no número de candidatos
-numero_candidatos = int(input())
+print("Eleição 2022")
+numero_candidatos = int(input("Houveram quantos candidatos para a eleição de 2022? "))
 
 # Leitura do nome dos candidatos
 nome_candidatos = []
+num_candidato = 1
+
 for i in range(0, numero_candidatos):
-    nome = input()
+    nome = input("Digite o nome do(a) candidato(a) " + str(num_candidato) + ": ")
+    num_candidato = num_candidato + 1
     nome_candidatos.append(nome)
+#print(nome_candidatos)
 
 # Leitura dos votos
 quantidade_votos = []
-for n in range(0, numero_candidatos+2):
-    votos = int(input())
+posicao_candidato = 0
+for n in range(0, numero_candidatos):
+    votos = int(input("Digite a quantidade de votos para o(a) canditado(a) " + nome_candidatos[posicao_candidato] + ": "))
+    posicao_candidato += 1
     quantidade_votos.append(votos)
-    
+
+brancos = int(input("Digite a quantidade de votos brancos: "))
+quantidade_votos.append(brancos)
+
+nulos = int(input("Digite a quantidade de votos nulos: "))
+quantidade_votos.append(nulos)
+
+#print(quantidade_votos)
+
 max_votos = max(quantidade_votos[0:-2])
 posicao_max = quantidade_votos.index(max_votos)
 vencedor = nome_candidatos[posicao_max]
 total_votos_validos = sum(quantidade_votos [:-2])
 
-
-
 # Impressão das informações de saída
-
-if max_votos >= total_votos_validos // 2:
+if max_votos >= (total_votos_validos // 2):
 # Se houve vencedor em primeiro turno
-    print(vencedor, "foi o vencedor da eleição")
+    print(vencedor, "foi o(a) vencedor(a) da eleição!")
 else:
 # Se é necessário segundo turno
-    print("Haverá segundo turno entre:")
     primeiro = vencedor
     lista_por_ordem = quantidade_votos.copy()
     lista_por_ordem.sort(reverse=True)
     segundo = nome_candidatos[quantidade_votos.index(lista_por_ordem[1])]
+    print("Haverá segundo turno entre: " + primeiro + " e " + segundo + ".")
     print(primeiro)
     print(segundo)
 
-
-
 total_votos = sum(quantidade_votos)
-print("Total de votos:", total_votos)
-
-print("Votos válidos:", total_votos_validos)
+print("A eleição se encerra com um total de", total_votos, "votos, sendo", total_votos_validos, "votos válidos.")
